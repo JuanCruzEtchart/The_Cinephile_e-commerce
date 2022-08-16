56ee32d5ec13223d11d32d9d9d98a6ee72b7329d
JuanCruzEtchart <etchart.juancruz@gmail.com>
2022-08-15 22:17:11 -0300

    Sprint 2 Agregado de estructura principal

--- app.js
+++ app.js
@@ -5,6 +5,26 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(3000, () => console.log("Servidor corriendo en puerto 3000"));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./views/home.html"));
+app.get("/inicio", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/inicio.html"));
+});
+
+app.get("/detalle", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/detalle.html"));
+});
+
+app.get("/carrito", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/carrito.html"));
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/login.html"));
+});
+
+app.get("/registro", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/registro.html"));
+});
+
+app.get("/perfil", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/perfil.html"));
 });
