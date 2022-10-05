a9df7cb41cbde92172f2fdab285fdcd010d614bd
Alberto <albertoropervilar@gmail.com>
2022-10-05 18:34:23 -0300

    push 10/5

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,8 @@
 const fs = require("fs");
 const { platform } = require("os");
 const path = require("path");
+const bcryptjs = require("bcryptjs");
+const {validationResult} = require("express-validator");
 
 function findAll(){
  const jsonData = fs.readFileSync(path.join(__dirname, "../data/users.json"))
@@ -18,30 +20,34 @@ const userController = {
 
   login: (req, res) => {
     const data = findAll();
-   //Encontrando el ID
-
-    /* const usuarioEncontrado = data.find(function(usuario){
-
-        return usuario.id == req.body
-      
-    })
-    */
     res.render("login", {users: data});
+
+    //validations
     },
 
     register: (req, res) => {
+
     const data = findAll();
+
     res.render("register", {users: data} );
+
+    //Validations
   },
     
     thankyou: (req, res) => {
-    const data = findAll();
-    res.render("thankyou", {users: data} );
+
+    res.render("thankyou");
     },
 
     store: (req, res) => {
       
-      console.log(req.body);
+     const errors = validationResult(req);
+     if(!errors.isEmpty())
+     {
+      console.log(errors.mapped());
+
+      return res.render("register", {errors: errors.mapped(), old: req.body});
+     }
       
       const data = findAll();
       
@@ -49,8 +55,8 @@ const userController = {
       id: data.length + 1,
       name: req.body.user,
       email: req.body.email,
-      password: req.body.password,
-      security: 1
+      password: bcryptjs.hashSync(req.body.password, 10),
+      security:"user"
       }
       console.log(newUser);
       data.push (newUser);
@@ -60,6 +66,23 @@ const userController = {
       res.redirect("/user/thankyou")
     },
 
+    check: (req, res) => {
+      console.log(req.body);
+      const data = findAll();
+      const userEncontrado = data.filter(function(user){
+        return (user.email == req.body.email && user.password == req.body.password) ;
+      })
+
+      if (userEncontrado!=undefined){
+        res.redirect("/index/id")
+      }  
+
+      else {
+        //validation errors
+      }
+  
+     
+    }
 
 };
 
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,15 +1,19 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController.js");
+const validation = require("../validations/userValidations");
 
 /*Render del login*/
 
 router.get("/login", userController.login);
+router.post("/login", userController.check);
 
 /*Render del register*/
 
 router.get("/register", userController.register);
-router.post("/register", userController.store);
+router.post("/register", validation.registerValidation, userController.store);
+
+/*Render del login*/
 router.get("/thankyou", userController.thankyou);
 
 module.exports = router;
