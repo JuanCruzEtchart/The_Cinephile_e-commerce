ce224df0e9be278962345d74d76d71e086e285ba
JuanCruzEtchart <etchart.juancruz@gmail.com>
2022-10-08 01:45:03 -0300

    Carga de productos finales

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,93 +2,81 @@ const fs = require("fs");
 const { platform } = require("os");
 const path = require("path");
 const bcryptjs = require("bcryptjs");
-const {validationResult} = require("express-validator");
+const { validationResult } = require("express-validator");
 
-function findAll(){
- const jsonData = fs.readFileSync(path.join(__dirname, "../data/users.json"))
+function findAll() {
+  const jsonData = fs.readFileSync(path.join(__dirname, "../data/users.json"));
   const data = JSON.parse(jsonData);
-  return data
-};
+  return data;
+}
 
-function stringAndCreate(data){
-  const dataString = JSON.stringify(data, null, 4)
+function stringAndCreate(data) {
+  const dataString = JSON.stringify(data, null, 4);
   fs.writeFileSync(path.join(__dirname, "../data/users.json"), dataString);
 }
 
-
 const userController = {
-
   login: (req, res) => {
     const data = findAll();
-    res.render("login", {users: data});
+    res.render("login", { users: data });
 
     //validations
-    },
+  },
 
-    register: (req, res) => {
-
+  register: (req, res) => {
     const data = findAll();
 
-    res.render("register", {users: data} );
+    res.render("register", { users: data });
 
     //Validations
   },
-  
+
   profile: (req, res) => {
     res.render("profile");
   },
 
-    
-    thankyou: (req, res) => {
-
+  thankyou: (req, res) => {
     res.render("thankyou");
-    },
+  },
 
-    store: (req, res) => {
-      
-     const errors = validationResult(req);
-     if(!errors.isEmpty())
-     {
+  store: (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
       console.log(errors.mapped());
 
-      return res.render("register", {errors: errors.mapped(), old: req.body});
-     }
-      
-      const data = findAll();
-      
-      const newUser = {
+      return res.render("register", { errors: errors.mapped(), old: req.body });
+    }
+
+    const data = findAll();
+
+    const newUser = {
       id: data.length + 1,
       name: req.body.user,
       email: req.body.email,
       password: bcryptjs.hashSync(req.body.password, 10),
-      security:"user"
-      }
-      console.log(newUser);
-      data.push (newUser);
-      
-      stringAndCreate(data);
+      security: "user",
+    };
+    console.log(newUser);
+    data.push(newUser);
 
-      res.redirect("/user/thankyou")
-    },
+    stringAndCreate(data);
 
-    check: (req, res) => {
-      console.log(req.body);
-      const data = findAll();
-      const userEncontrado = data.filter(function(user){
-        return (user.email == req.body.email && user.password == req.body.password) ;
-      })
+    res.redirect("/user/thankyou");
+  },
 
-      if (userEncontrado!=undefined){
-        res.redirect("/index/id")
-      }  
+  check: (req, res) => {
+    console.log(req.body);
+    const data = findAll();
+    const userEncontrado = data.filter(function (user) {
+      return user.email == req.body.email && user.password == req.body.password;
+    });
 
-      else {
-        //validation errors
-      }
-  
-     
+    if (userEncontrado != undefined) {
+      res.redirect("/index/id");
+    } else {
+      //validation errors
     }
-
+  },
 };
 
 module.exports = userController;
